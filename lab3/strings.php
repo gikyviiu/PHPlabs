0f25b47c0856e18a42b39fe8e03a78b84e164fcc
deeanah <pshhh.sh@ya.ru>
2024-11-08 20:12:09 +0300

    7

---
+++ lab3/strings.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+$login = ' User ';
+$password = 'megaP@ssw0rd';
+$name = 'иван';
+$email = 'ivan@petrov.ru';
+$code = '<?=$login?>'
+	
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Использование функций обработки строк</title>
+</head>
+<body>
+
+<?php
+function isDifficultPass($password){
+	return preg_match('/[A-Z]/', $password) &&
+			preg_match('/[a-z]/', $password) &&
+			preg_match('/[0-9]/', $password) && 
+			strlen($password) >= 8;
+}
+
+
+
+$login = strtolower(trim($login));
+$passIsValid = isDifficultPass($password) ? 'Сложный пароль' : 'Уязвимый пароль';
+$name = mb_convert_case($name, MB_CASE_TITLE, 'UTF-8');
+$emailValid = filter_var($email, FILTER_VALIDATE_EMAIL) ? 'email корректный' : 'email некорректный';
+
+echo htmlspecialchars($code);
+
+?>
+</body>
+</html>
