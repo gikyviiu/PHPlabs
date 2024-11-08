1f280c1fdc7731a6c36de4e1b176e4884f9f64ec
deeanah <pshhh.sh@ya.ru>
2024-11-08 20:22:39 +0300

    4

---
+++ lab3/constants.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Константы PHP</title>
+</head>
+
+<body>
+	<h3>Константы PHP</h3>
+	<pre>
+	<?php
+        print_r(get_defined_constants(true));
+	?>
+	</pre>
+</body>
+
+</html>
