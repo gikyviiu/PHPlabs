5e75723a1b2654de0c3111f93ed2f95dd3e27397
deeanah <pshhh.sh@ya.ru>
2024-11-08 21:41:00 +0300

    3

--- lab4/site/calc.php
+++ lab4/site/calc.php
@@ -1,58 +1,72 @@
+<?php
+
+$result = null;
+$num1 = null;
+$num2 = null;
+$operator = '+';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $num1 = filter_input(INPUT_POST, 'num1', FILTER_VALIDATE_FLOAT);
+    $num2 = filter_input(INPUT_POST, 'num2', FILTER_VALIDATE_FLOAT);
+    $operator = $_POST['operator'];
+}
+
+if($num1 !== false && $num2 !== false){
+    switch($operator){
+        case '+': 
+            $result = $num1 + $num2;
+            break;
+        case '-': 
+            $result = $num1 - $num2;
+            break;
+        case '*': 
+            $result = $num1 * $num2;
+            break;
+        case '/': 
+            if($num2 == 0) $result = "На ноль делить нельзя!";
+            else $result = $num1 / $num2;
+            break;
+        default:
+            echo 'Неккоректный оператор';
+    }
+} else $result = 'Введите корректные значения';
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Калькулятор</title>
-  <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Калькулятор</title>
 </head>
 <body>
-  <header>
-    <!-- Верхняя часть страницы -->
-    <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-  </header>
-  <section>
-    <!-- Заголовок -->
-    <h1>Калькулятор школьника</h1>
-    <!-- Заголовок -->
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Число 1:</label>
-      <br>
-      <input name='num1' type='text'>
-      <br>
-      <label>Оператор: </label>
-      <br>
-      <input name='operator' type='text'>
-      <br>
-      <label>Число 2: </label>
-      <br>
-      <input name='num2' type='text'>
-      <br>
-      <br>
-      <input type='submit' value='Считать'>
-    </form>
-    <!-- Область основного контента -->
-  </section>
-  <nav>
-    <h2>Навигация по сайту</h2>
-    <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a></li>
-      <li><a href='about.php'>О нас</a></li>
-      <li><a href='contact.php'>Контакты</a></li>
-      <li><a href='table.php'>Таблица умножения</a></li>
-      <li><a href='calc.php'>Калькулятор</a></li>
-    </ul>
-    <!-- Меню -->
-  </nav>
-  <footer>
-    <!-- Нижняя часть страницы -->
-    &copy; Супер Мега Веб-мастер, 2000 &ndash; 20xx
-    <!-- Нижняя часть страницы -->
-  </footer>
+
+<h1>Калькулятор</h1>
+
+<?php
+
+if($result != null) echo "Результат: $num1 $operator $num2 = $result" . '<hr>';
+?>
+
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+
+<p><label for="num1">Число 1</label><br>
+<input type="text" name="num1" id="num1" required></p>
+
+<p><label for="operator">Оператор</label><br>
+<select name="operator" id="operator">
+    <option value="+" selected >+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+</select></p>
+
+<p><label for="num2">Число 2</label><br>
+<input type="text" name="num2" id="num2" required></p>
+
+<button type="submit">Считать-></button>
+
+</form>
+
 </body>
 </html>
--- lab4/site/inc/lib.inc.php
+++ lab4/site/inc/lib.inc.php
@@ -1,11 +1,21 @@
 <?php
 declare(strict_types=1);
 
-function getTable(int $cols = 7, int $rows = 7, string $color = 'orange'): int
+function getMenu(array $menu, bool $vertical = true): void
+{
+    if($vertical) echo '<ul class="menu">';
+    else echo '<ul class="menu vertical">';
+        foreach ($menu as $item) {
+            echo '<li><a href="' . $item['href'] . '">' . $item['link'] . '</a></li>';
+        }
+    echo '</ul>';
+}
+
+function drawTable(int $cols = 10, int $rows = 10, string $color = 'pink'): int
 {
 	static $count = 0;
 	$count++;
-	$table = '<table>';
+	$table = '<table border="1" width="200"><tbody>';
 	for ($i = 1; $i <= $cols; $i++) {
 		$table .= '<tr>';
 		for ($j = 1; $j <= $rows; $j++) {
@@ -17,20 +27,7 @@ function getTable(int $cols = 7, int $rows = 7, string $color = 'orange'): int
 		}
 		$table .= '</tr>';
 	}
-	$table .= '</table>';
+	$table .= '</tbody></table>';
 	echo $table;
 	return $count;
-}
-
-
-
-
-function getMenu(array $menu, bool $vertical = true): void
-{
-    if($vertical) echo '<ul class="menu">';
-    else echo '<ul class="menu vertical">';
-        foreach ($menu as $item) {
-            echo '<li><a href="' . $item['href'] . '">' . $item['link'] . '</a></li>';
-        }
-    echo '</ul>';
 }
