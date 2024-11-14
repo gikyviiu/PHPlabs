5b5ee5d689c80d6bd00306d5f9efabdb39483ec5
gikyviiu <pshhh.sh@ya.ru>
2024-11-14 16:52:58 +0300

    324

--- lab5/file.php
+++ lab5/file.php
@@ -1,23 +1,11 @@
 <?php
 declare(strict_types=1);
 
-/*
-ЗАДАНИЕ 1
-- Установите константу для хранения имени файла
-- Проверьте, отправлялась ли форма и корректно ли отправлены данные из формы
-- В случае, если форма была отправлена, отфильтруйте полученные значения 
-  с помощью функций trim(), htmlspecialchars()
-- Сформируйте строку для записи с файл
-- Откройте соединение с файлом и запишите в него сформированную строку
-- Используя функцию header() выполните перезапрос текущей страницы 
-  (чтобы избавиться от данных, отправленных методом POST)
-*/
-
-define('FILENAME', 'users.txt');
+define('FILENAME', 'guests.txt');
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['fname'], $_POST['lname'])) {
-  // фильтруем полученные значения
+  
   $fname = trim(htmlspecialchars($_POST['fname']));
   $lname = trim(htmlspecialchars($_POST['lname']));
 
@@ -25,7 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['fname'], $_POST['lname
   $line = "$fname $lname\n";
   file_put_contents(FILENAME, $line, FILE_APPEND);
 
-  // Перезапрос текущей страницы, чтобы избавиться от данных POST
   header("Location: " . $_SERVER['PHP_SELF']);
   exit;
 }
@@ -56,14 +43,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['fname'], $_POST['lname
   </form>
 
   <?php
-  /*
-  ЗАДАНИЕ 2
-  - Проверьте, существует ли файл с информацией о пользователях
-  - Если файл существует, получите все содержимое файла в виде массива строк 
-    с помощью функции file()
-  - В цикле выведите все строки данного файла с порядковым номером строки
-  - После этого выведите размер файла в байтах.
-  */
 
   if (file_exists(FILENAME)) {
 
